ffb35b64cc6dad9daa6ecd929cef88f17ae41b9d
ArtemDRUZHBA <152015055+ArtemDRUZHBA@users.noreply.github.com>
2025-09-08 12:38:39 +0300

    Delete NuzzleTrigger

--- Assets/Scipts/Game/Nozzle/NozzleTrigger.cs
+++ Assets/Scipts/Game/Nozzle/NozzleTrigger.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class NozzleTrigger : MonoBehaviour
 {
+    [Header("Триггеры")]
     [SerializeField] private Nozzle _nozzle;
     [SerializeField] private Collider _fuelSocketTrigger;
     [SerializeField] private Collider _nozzleHandleTrigger;
+
+    [Header("XR Snap Targets")]
+    [SerializeField] private Transform handTarget;
+    [SerializeField] private Transform tankTarget;
+    [SerializeField] private Transform socketTarget;
+
+    [Header("XR Snap Parents")]
+    [SerializeField] private Transform handParent;
+    [SerializeField] private Transform tankParent;
+    [SerializeField] private Transform socketParent;
+
+    [Header("Snapper")]
+    [SerializeField] private XRObjectSnapper snapper;
 
     private void Start()
     {
@@ -18,20 +32,26 @@ public class NozzleTrigger : MonoBehaviour
             Debug.Log("Джойстик взял пистолет");
             _fuelSocketTrigger.isTrigger = true;
             _nozzleHandleTrigger.isTrigger = false;
-            //вызов встроенного метода XR с взятием, а именно взятием этого пистолета
+
+            transform.SetParent(other.transform);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
         }
-        if (other.CompareTag("FuelSocket"))
+        else if (other.CompareTag("FuelSocket"))
         {
             Debug.Log("Джойстик вернул пистолет в гнездо");
             _fuelSocketTrigger.isTrigger = false;
             _nozzleHandleTrigger.isTrigger = true;
-            _nozzle.ReturnToRest();
+
+            transform.SetParent(other.transform);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
         }
-        if (other.CompareTag("FuelTank"))
+        else if (other.CompareTag("FuelTank"))
         {
             Debug.Log("Джойстик вставил пистолет в бак");
             _nozzleHandleTrigger.isTrigger = true;
-            //вызов метода с перемещеним пистолета в дочерний элемент этого бака и изменением позиции
+            //snapper.SnapObject(tankTarget, tankParent);
         }
     }
 }
