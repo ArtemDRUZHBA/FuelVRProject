441ba0f7168f9b6a8dc658b3c0f733063c080552
ArtemDRUZHBA <152015055+ArtemDRUZHBA@users.noreply.github.com>
2025-09-09 07:34:07 +0300

    Add and fix model NozzleTrigger

--- Assets/Scipts/Game/Nozzle/NozzleTrigger.cs
+++ Assets/Scipts/Game/Nozzle/NozzleTrigger.cs
@@ -2,56 +2,39 @@ using UnityEngine;
 
 public class NozzleTrigger : MonoBehaviour
 {
-    [Header("Триггеры")]
-    [SerializeField] private Nozzle _nozzle;
-    [SerializeField] private Collider _fuelSocketTrigger;
-    [SerializeField] private Collider _nozzleHandleTrigger;
+    [Header("Теги для вставки")]
+    [SerializeField] private string fuelSocketTag = "FuelSocket";
+    [SerializeField] private string fuelTankTag = "FuelTank";
 
-    [Header("XR Snap Targets")]
-    [SerializeField] private Transform handTarget;
-    [SerializeField] private Transform tankTarget;
-    [SerializeField] private Transform socketTarget;
+    [Header("Точки вставки")]
+    [SerializeField] private Transform socketInsertPoint;
+    [SerializeField] private Transform tankInsertPoint;
 
-    [Header("XR Snap Parents")]
-    [SerializeField] private Transform handParent;
-    [SerializeField] private Transform tankParent;
-    [SerializeField] private Transform socketParent;
-
-    [Header("Snapper")]
-    [SerializeField] private XRObjectSnapper snapper;
-
-    private void Start()
-    {
-        _fuelSocketTrigger.isTrigger = false;
-        _nozzleHandleTrigger.isTrigger = true;
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("NozzleHandle"))
+        if (other.CompareTag(fuelSocketTag) && socketInsertPoint != null)
         {
-            Debug.Log("Джойстик взял пистолет");
-            _fuelSocketTrigger.isTrigger = true;
-            _nozzleHandleTrigger.isTrigger = false;
+            SnapTo(socketInsertPoint);
+            Debug.Log("Пистолет возвращён в гнездо");
+        }
 
-            transform.SetParent(other.transform);
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-        }
-        else if (other.CompareTag("FuelSocket"))
+        else if (other.CompareTag(fuelTankTag) && tankInsertPoint != null)
         {
-            Debug.Log("Джойстик вернул пистолет в гнездо");
-            _fuelSocketTrigger.isTrigger = false;
-            _nozzleHandleTrigger.isTrigger = true;
+            SnapTo(tankInsertPoint);
+            Debug.Log("Пистолет вставлен в бак");
+        }
+    }
 
-            transform.SetParent(other.transform);
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-        }
-        else if (other.CompareTag("FuelTank"))
-        {
-            Debug.Log("Джойстик вставил пистолет в бак");
-            _nozzleHandleTrigger.isTrigger = true;
-            //snapper.SnapObject(tankTarget, tankParent);
-        }
+    private void SnapTo(Transform target)
+    {
+        transform.SetParent(target);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        var rb = GetComponent<Rigidbody>();
+        if (rb != null) rb.isKinematic = true;
+
+        //var col = GetComponent<Collider>();
+        //if (col != null) col.enabled = false;
     }
 }
