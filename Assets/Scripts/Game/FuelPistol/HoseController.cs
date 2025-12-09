3fa63ed2ca32c6433d4551147841a3128b6dde35
ArtemDRUZHBA <152015055+ArtemDRUZHBA@users.noreply.github.com>
2025-12-09 12:41:29 +0300

    Outline

--- Assets/Scripts/Game/FuelPistol/HoseController.cs
+++ Assets/Scripts/Game/FuelPistol/HoseController.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HoseController : MonoBehaviour
 {
-    public Transform[] bones;     // кости шланга
+    public Transform[] bones;       // кости шланга
+    public Transform anchorPoint;   // точка крепления (начало)
+    public Transform hoseEndPoint;  // точка выхода из пистолета/колонки
 
-    public Transform anchorPoint; // точка крепления (начало)
-    public Transform hoseEndPoint;   // точка выхода из пистолета/колонки
+    private Rigidbody firstRb;
+    private Rigidbody lastRb;
 
-    private void Update()
+    void Start()
     {
-        if (bones.Length > 0 && hoseEndPoint != null)
+        if (bones.Length > 0)
         {
-            Rigidbody lastRb = bones[bones.Length - 1].GetComponent<Rigidbody>();
+            firstRb = bones[0].GetComponent<Rigidbody>();
+            lastRb = bones[bones.Length - 1].GetComponent<Rigidbody>();
+
+            // фиксируем первую кость
+            firstRb.isKinematic = true;
+            bones[0].position = anchorPoint.position;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (lastRb != null && hoseEndPoint != null)
+        {
+            // двигаем последнюю кость к пистолету
             lastRb.MovePosition(hoseEndPoint.position);
         }
     }
